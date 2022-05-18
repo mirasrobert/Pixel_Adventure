2a5bb591e9526635db83ca49afeb0a6f2cd95f7e
mirasrobert <mrmirasrobert@gmail.com>
2022-05-18 12:48:47 +0800

    Level 1 Complete

--- Assets/Scripts/Player/DoorKey.cs
+++ Assets/Scripts/Player/DoorKey.cs
@@ -1,21 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
 
 public class DoorKey : MonoBehaviour
 {
+
+    public TextMeshProUGUI doorIsLockText;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             bool playerHasTheKey = collision.GetComponent<Stats>().canEnter();
             if(playerHasTheKey)
-            { // Finish Level
-                print("Level 1 Complete");
+            {   // Finish Level
+                SceneManager.LoadScene("Gameover");
             } else
             {
-                print("Door Locked, No Key");
+                doorIsLockText.text = "Door is lock";
+                StartCoroutine(hideText(1));
             }
         }
+    }
+
+    IEnumerator hideText(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        doorIsLockText.text = "";
     }
 }
